757540c7b1fe93515a0b1e87aa028f7b35b70265
Aziret-Ali Ismailov <ismailovisaac@gmail.com>
2025-05-23 17:14:16 -0400

    Implement function to get human choice

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function getComputerChoice(){
         return 'rock';
     }
 }
+
+function getHumanChoice(){
+    let choice = parseInt(prompt("Please enter the number corresponding to your choice: scissors - 0; paper - 1; rock - 2 : "));
+    if(choice == 0){
+        return 'scissors';
+    }else if(choice==1){
+        return 'paper';
+    }else{
+        return 'rock';
+    }
+}
+console.log(getHumanChoice());
